4edf99e16abec0f9aa03d62e31791ae7b1cc3ebe
Nicola <nyc.zyp@gmail.com>
2014-11-09 18:14:17 +0000

    pag. 035

--- 01_cards.c
+++ 01_cards.c
@@ -1,28 +1,37 @@
 #include <stdio.h>
-#include <stdio.h>
+#include <stdlib.h>
 
 int main () {
 	char card_name[3];
-	puts ("Enter the card_name: ");
-	scanf ("%2s", card_name);
-	int val = 0;
-	if (card_name[0] == 'K') {
-		val = 10;
-	} else if (card_name[0] == 'Q') {
-		val = 10;
-	} else if (card_name[0] == 'J') {
-		val = 10;
-	} else if (card_name[0] == 'A') {
-		val = 11;
-	} else  {
-		val = atoi(card_name);
+	int count = 0;
+	while (card_name[0] != 'X') {
+		puts ("Enter the card_name: ");
+		scanf ("%2s", card_name);
+		int val = 0;
+		switch (card_name[0]) {
+		case 'K':
+		case 'Q':
+		case 'J':
+			val = 10;
+			break;
+		case 'A':
+			val = 11;
+			break;
+		case 'X':
+			return 0;
+		default:
+			val = atoi(card_name);
+			if ((val < 1) || (val > 10)) {
+				puts ("The card is out of range");
+			//	continue;
+			}
+		}
+		if ((val > 2) && (val < 7)) {
+			count++;
+		} else if (val == 10) {
+			count--;
+		}
+		printf("Current count: %i\n", count);
 	}
-	/* Check if the values is 3 to 6 */
-	if ((val > 2) && (val < 7))
-		puts("Count has gone up");
-	/* Otherwise check if the card was 10, J, Q, or K */
-	else if (val == 10)
-		puts ("Count has gone down");
-//	printf("The card value is: %i\n", val);
 	return 0;
 }
