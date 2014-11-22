12416f48a3a994f07bbf1d7308f9282949479752
Nicola <nyc.zyp@gmail.com>
2014-11-22 22:18:36 +0000

    pag. 146

--- 08a_Categorize.c
+++ 08a_Categorize.c
@@ -2,16 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main() {
+int main(int argc, char *argv[]) {
 	char line[80];
+	if (argc != 6) {
+		fprintf(stderr, "You need to give 5 arguments\n");
+		return 1;
+	}
 	FILE *in = fopen("07b_spooky.csv", "r");
-	FILE *file1 = fopen("08b_ufos.csv", "w");
-	FILE *file2 = fopen("08c_disappearances.csv", "w");
-	FILE *file3 = fopen("08d_others.csv", "w");
+	FILE *file1 = fopen(argv[2], "w");
+	FILE *file2 = fopen(argv[4], "w");
+	FILE *file3 = fopen(argv[5], "w");
 	while (fscanf(in, "%79[^\n]\n", line)==1) {
-		if (strstr(line, "UFO"))
+		if (strstr(line, argv[1]))
 			fprintf(file1, "%s\n", line);
-		else if (strstr(line,"Disappearance"))
+		else if (strstr(line,argv[3]))
 			fprintf(file2, "%s\n", line);
 		else
 			fprintf(file3, "%s\n", line);
